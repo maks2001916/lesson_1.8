8df682149fd1427a9fb7611aa081dcbf359abe9a
maks <mamonov.maksim.20003@gmail.com>
2022-08-23 21:21:47 +0500

    Первое задание выполнено.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.time.LocalDate;
+
 public class Main {
+    //Первое задание
+    public static int findingALeapYear(int currentYear) {
+        int variable = 0;
+        if (currentYear % 4 == 0 && currentYear % 100 != 0 || currentYear % 400 == 0) {
+             variable = 0;
+        } else {
+             variable = 1;
+        }
+        return variable;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int currentYear = LocalDate.now().getYear();
+        findingALeapYear(currentYear);
+        int result = findingALeapYear(currentYear);
+        if (result == 0) {
+            System.out.println(currentYear + " - високосный год");
+        } else {
+            System.out.println(currentYear + " - не високосный год");
+        }
     }
 }
